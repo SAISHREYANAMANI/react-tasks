6936578e9bec9239d50cf5981ebd85c20523d510
SAISHREYANAMNI <shreyanamani@gmail.com>
2024-03-08 17:06:02 +0530

    productstask added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import Controlled from "./components/functionalcomponents/forms/controlled";
 import Registration from "./components/functionalcomponents/forms/registrationform";
 import Uncontrolled from "./components/functionalcomponents/forms/uncontrolled";
+import UseEffect4 from "./components/functionalcomponents/hooks/useEffect/useEffectEx4";
+import UseEffectExample from "./components/functionalcomponents/hooks/useEffect/useEffectEx1";
+import UseEffectEx2 from "./components/functionalcomponents/hooks/useEffect/useEffectEx2";
+import UseEffect3Example from "./components/functionalcomponents/hooks/useEffect/useEffectEx3";
+import UseEffectTask from "./components/functionalcomponents/hooks/useEffect/useEffectTaks";
 
 
 
@@ -12,7 +17,13 @@ function App() {
     <div>
  {/* <Uncontrolled/> */}
  {/* <Controlled/> */}
- <Registration/>
+ {/* <Registration/> */}
+{/* < UseEffectExample/> */}
+{/* <UseEffectEx2/> */}
+{/* <UseEffect3Example/> */}
+{/* <UseEffect4/> */}
+<UseEffectTask/>
+
 
 
     </div>
---
+++ src/components/functionalcomponents/hooks/useEffect/useEffectEx2.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Spinner } from "react-bootstrap"
+const UseEffectEx2=()=>{
+    const[count,setCount]=useState(0)
+    const [data,setData]=useState([])
+    useEffect(() => {     
+         fetchData()
+      },[]);
+    const fetchData = async ()=>{
+    const result =await axios.get("https://dummyjson.com/products/1")
+      setData(result.data.products)
+
+    }
+    
+    return(
+        <>
+        <h1>UseEffect</h1>
+        <h4>{count}</h4>
+        <button  onClick={()=>setCount(count+1)} >Count change</button>
+        {
+            data.length>0
+            ?
+            data.map(item=><h4>{item.title}</h4>)
+            :
+            <Spinner/>
+        }
+        </>
+    )
+}
+export default UseEffectEx2
