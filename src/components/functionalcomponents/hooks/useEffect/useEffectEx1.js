f8fe2ab2f7ce8456bb9961aecd1bf2b0b66c238b
SAISHREYANAMNI <shreyanamani@gmail.com>
2024-03-11 18:30:45 +0530

    useeffectatskadded

--- src/components/functionalcomponents/hooks/useEffect/useEffectEx1.js
+++ src/components/functionalcomponents/hooks/useEffect/useEffectEx1.js
@@ -30,3 +30,15 @@ const UseEffectExample=()=>{
     )
 }
 export default UseEffectExample
+
+
+// depndency array :
+// based on the depencey array , useEffect will go for different life cyles 
+
+// 1. remove the array ---> useEffect re-renders for every state or prop changes 
+
+
+// 2. empty array --> useEffect will act like a component did mount 
+// 3. array with state or prop values --> useEffect will act like a component did update 
+// 4. if callback returns the function we can prevent memory leaks ----> useEffect will act like a component 
+// willUnmount 
