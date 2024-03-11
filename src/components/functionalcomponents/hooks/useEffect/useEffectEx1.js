bf54c3c981a42700f14d22957b95829eeae2ab1f
SAISHREYANAMNI <shreyanamani@gmail.com>
2024-03-11 18:29:11 +0530

    useeffectatsk

--- src/components/functionalcomponents/hooks/useEffect/useEffectEx1.js
+++ src/components/functionalcomponents/hooks/useEffect/useEffectEx1.js
@@ -1,42 +1,44 @@
-
-
-// hooks : hooks let you use of state and other react features(life cycle methods) with out writing the class 
-
-
-// useEffect :
-// useEffect is the hook in the functional based components
-// which is used to perform the side effects 
-
-// useEffect is replica of 3 life cycle methods in class based components 
-
-// 1. ComponentDidMount 
-// 2. ComponentDidUpdate 
-// 3. ComponentWillUnmount 
-
-// useEffect will aceepts 2 args 
-
-// 1. callback function 
-// 2. array  --> 
-
-
-// useEffect(()=>{},[])
-
-
-// which is used to perform the side effects 
-
-// 1. data fetching 
-// 2. dom manipulations 
-// 3. timers and third party subscribtions 
-// 4. event listeners 
+import { useEffect, useState } from "react"
 
 
 
+const UseEffectExample=()=>{
+    const[counter,setCounter]=useState(0)
+    const[counter1,setCounter1]=useState(10)
+
+    useEffect(()=>{
+        console.log("useEffect")
+    },[counter,counter1])
+
+    const handleIncrement =()=>{
+        setCounter(counter+1)
+    }
+    const handleIncrement2 =()=>{
+        setCounter1(counter1+1)
+    }
+
+    return(
+        <>     
+        <h1>Counter {counter}</h1>
+        <button onClick={handleIncrement} >Increment</button>
 
 
+        <h1>Counter  {counter1}</h1>
+    
+        <button onClick={handleIncrement2} >Increment counter 2</button>
+        </>
+    )
+}
+export default UseEffectExample
 
 
+// depndency array :
+// based on the depencey array , useEffect will go for different life cyles 
+
+// 1. remove the array ---> useEffect re-renders for every state or prop changes 
 
 
-
-
-
+// 2. empty array --> useEffect will act like a component did mount 
+// 3. array with state or prop values --> useEffect will act like a component did update 
+// 4. if callback returns the function we can prevent memory leaks ----> useEffect will act like a component 
+// willUnmount 
